8dd72a79bab86d68e7350885da1384c632089561
greyfoss <yinjw@bielcrystal.com>
2021-09-11 11:45:47 +0800

    modify transformer

--- bm25/main.py
+++ bm25/main.py
@@ -5,14 +5,14 @@ from bm25.okapi_bm25 import OkapiBM25
 
 def main():
     data_path = './data/questions.csv'
-    df = pd.read_csv(data_path, usecols=['content'])
-    corpus = df['content'].tolist()
+    df = pd.read_csv(data_path, header=None)
+    corpus = df[0].tolist()
 
     bm25 = OkapiBM25(corpus, tokenize=jieba.lcut)
 
-    query = "医生，我肛门处非常痒怎么办"
+    query = "取钱要不要扣钱啊"
 
-    result = bm25.get_top_k(query, corpus=corpus, k=5)
+    result = bm25.get_top_k(query, corpus=corpus, k=6)
 
     for q in result:
         print(q)
--- transformer/transformer.py
+++ transformer/transformer.py
@@ -407,6 +407,7 @@ def run_epoch(data_iter, model, loss_compute):
     tokens = 0
     for i, batch in enumerate(data_iter):
         out = model.forward(batch.src, batch.trg, batch.src_mask, batch.trg_mask)
+        # 计算loss
         loss = loss_compute(out, batch.trg_y, batch.ntokens)
         total_loss += loss
         total_tokens += batch.ntokens
@@ -509,6 +510,23 @@ class SimpleLossCompute:
             self.opt.optimizer.zero_grad()
 
         return loss.data * norm
+
+
+global max_src_in_batch, max_tgt_in_batch
+
+
+def batch_size_fn(new, count, sofar):
+    "Keep augmenting batch and calculate total number of tokens + padding."
+    # 保持增大批大小并计算token + 填充的总数
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
 
 
 if __name__ == '__main__':
